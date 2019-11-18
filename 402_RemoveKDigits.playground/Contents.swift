3e7235f4197073a5baef33d7c635a753be94de9b
OnlyStu <371706801@qq.com>
2019-11-19 00:12:57 +0800

    add 402

---
+++ 402_RemoveKDigits.playground/Contents.swift
@@ -0,0 +1,59 @@
+
+/// 解法1:
+/// 思路：从首页开始比较，相邻数，谁大移除谁
+class Solution {
+    func removeKdigits(_ num: String, _ k: Int) -> String {
+        var stack:[Character] = []
+        var deleteIndex:Int = 0
+        
+        if k == 0 {
+            return num
+        }
+        
+        if num.count == k {
+            return "0"
+        }
+        
+        for char in num {
+            if stack.isEmpty {
+                stack.append(char)
+            }else{
+                stack.append(char)
+                
+                if k > deleteIndex {// 在删除次数没有达到时，数组里面只会保存两个数字，比较，移除最大的
+                    let first = Int(String(stack.first!))!
+                    let last = Int(String(stack.last!))!
+                    if first > last {
+                        stack.remove(at: 0)
+                        deleteIndex += 1
+                    }else if last > first{
+                        stack.popLast()
+                        deleteIndex += 1
+                    }
+                }
+            }
+        }
+        
+        if deleteIndex == 0 {
+            while deleteIndex < 3 {
+                stack.popLast()
+                deleteIndex += 1
+            }
+        }
+        
+        while Int(String(stack.first!))! == 0 && stack.count > 1 {//数组大于1，第一位是0的都需要移除
+            stack.remove(at: 0)
+        }
+        
+        return String(stack)
+    }
+}
+
+let so = Solution()
+so.removeKdigits("1432219", 3)
+so.removeKdigits("10200", 1)
+so.removeKdigits("10", 1)
+so.removeKdigits("112", 1)
+so.removeKdigits("0", 0)
+so.removeKdigits("1111111", 3)
+
